6faa1f3127e1d0fee011dd80fffa346b3c4051b0
CHANN <chann@chann.kr>
2015-08-05 20:33:04 +0900

    fix email auth.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'chann.kr' }
+
 end
