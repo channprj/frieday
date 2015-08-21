89482bff94ba5416dff7649b272a9e525964345f
helloeun <hello_eun11@naver.com>
2015-08-21 22:04:39 +0900

    add #18: 장바구니 기능 추가ver1, db seed, migrate 추가됨.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818113716) do
+ActiveRecord::Schema.define(version: 20150820163541) do
 
   create_table "bulletins", force: true do |t|
     t.string   "title"
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 20150818113716) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "post_type",   default: "bulletin"
-  end
-
-  create_table "chickens", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "price"
-    t.string   "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
@@ -38,6 +29,37 @@ ActiveRecord::Schema.define(version: 20150818113716) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+
+  create_table "order_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.decimal  "unit_price",  precision: 12, scale: 3
+    t.integer  "quantity"
+    t.decimal  "total_price", precision: 12, scale: 3
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
+  add_index "order_items", ["product_id"], name: "index_order_items_on_product_id", using: :btree
+
+  create_table "order_statuses", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orders", force: true do |t|
+    t.decimal  "subtotal",        precision: 12, scale: 3
+    t.decimal  "tax",             precision: 12, scale: 3
+    t.decimal  "shipping",        precision: 12, scale: 3
+    t.decimal  "total",           precision: 12, scale: 3
+    t.integer  "order_status_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "orders", ["order_status_id"], name: "index_orders_on_order_status_id", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -50,7 +72,10 @@ ActiveRecord::Schema.define(version: 20150818113716) do
 
   add_index "posts", ["bulletin_id"], name: "index_posts_on_bulletin_id", using: :btree
 
-  create_table "quickmenus", force: true do |t|
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.decimal  "price",      precision: 12, scale: 3
+    t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -79,16 +104,13 @@ ActiveRecord::Schema.define(version: 20150818113716) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "weekly_ranks", force: true do |t|
-    t.integer "total_orders"
-    t.integer "chicken_id"
-  end
-
-  add_index "weekly_ranks", ["chicken_id"], name: "index_weekly_ranks_on_chicken_id", using: :btree
 
 end
